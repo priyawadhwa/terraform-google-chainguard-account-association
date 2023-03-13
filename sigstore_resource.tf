578e928f2f10a24ea504e507af061c37beb16b02
Priya Wadhwa <priya@chainguard.dev>
2023-03-13 13:32:04 -0700

    Update cert authority name

--- sigstore_resource.tf
+++ sigstore_resource.tf
@@ -3,6 +3,6 @@ resource "chainguard_sigstore" "sigstore_ca" {
   parent_id   = "66fe2f6b69fb795e63abc2769c294ee0d049ee0a"
   description = "CA created by terraform"
   google_ca {
-    ref = "projects/priya-chainguard/locations/us-west1/caPools/priya-sigstore/certificateAuthorities/sigstore-test"
+    ref = "projects/priya-chainguard/locations/us-west1/caPools/priya-sigstore/certificateAuthorities/sigstore-authority"
   }
 }
