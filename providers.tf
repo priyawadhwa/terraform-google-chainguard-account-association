6ea9822e95207fff0906b55a6f27b6e038f2dffa
Priya Wadhwa <priya@chainguard.dev>
2023-03-13 13:43:04 -0700

    Switch from staging to priya-chainguard

--- providers.tf
+++ providers.tf
@@ -14,5 +14,5 @@ terraform {
 
 
 provider "chainguard" {
-  console_api = "https://console-api.chainops.dev"
+  console_api = "https://console-api.priya-chainguard.dev"
 }
--- sigstore_resource.tf
+++ sigstore_resource.tf
@@ -1,6 +1,6 @@
 resource "chainguard_sigstore" "sigstore_ca" {
   name        = "terraform-ca"
-  parent_id   = "66fe2f6b69fb795e63abc2769c294ee0d049ee0a"
+  parent_id   = var.github_actions_group_id
   description = "CA created by terraform"
   google_ca {
     ref = "projects/priya-chainguard/locations/us-west1/caPools/priya-sigstore/certificateAuthorities/sigstore-authority"
