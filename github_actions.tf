912c382cabaca900b4593a9d897c1539667b9be7
Priya Wadhwa <priya@chainguard.dev>
2023-03-23 13:27:32 -0700

    create chainguard group to store sigstore resources under

--- github_actions.tf
+++ github_actions.tf
@@ -1,10 +1,16 @@
+resource "chainguard_group" "dogfood-group" {
+  name        = "gha-signing-group"
+  description = "A group to hold sigstore resources in"
+  parent_id   = var.github_actions_group_id
+}
+
 # Look up the id for the sigstore.create role within this environment.
 data "chainguard_roles" "editor" {
   name = "signing.editor"
 }
 # Create the identity we will assume to push images from actions.
 resource "chainguard_identity" "actions-signer" {
-  parent_id   = var.github_actions_group_id
+  parent_id   = chainguard_group.dogfood-group.id
   name        = "actions signer"
   description = <<EOF
     This is the identity that we will assume from github actions to sign images.
@@ -25,6 +31,6 @@ output "signer-identity" {
 # Grant the identity the "registry.pus" role on our root group.
 resource "chainguard_rolebinding" "sign-stuff" {
   identity = chainguard_identity.actions-signer.id
-  group    = var.github_actions_group_id
+  group    = chainguard_group.dogfood-group.id
   role     = data.chainguard_roles.editor.items[0].id
 }
--- sigstore_resource.tf
+++ sigstore_resource.tf
@@ -1,6 +1,6 @@
 resource "chainguard_sigstore" "sigstore_ca" {
   name        = "terraform-ca"
-  parent_id   = var.github_actions_group_id
+  parent_id   = chainguard_group.dogfood-group.id
   description = "CA created by terraform"
   google_ca {
     ref = "projects/priya-chainguard/locations/us-west1/caPools/priya-sigstore/certificateAuthorities/sigstore-authority"
@@ -9,7 +9,7 @@ resource "chainguard_sigstore" "sigstore_ca" {
 
 resource "chainguard_sigstore" "sigstore_ca_tf" {
   name        = "terraform-ca-2"
-  parent_id   = var.github_actions_group_id
+  parent_id   = chainguard_group.dogfood-group.id
   description = "CA created by terraform"
   google_ca {
     ref = google_privateca_certificate_authority.ca.name
